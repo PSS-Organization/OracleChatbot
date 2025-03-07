a80958b4b63f4519e70041b714de0236291dc0a8
valentino-vm <131170839+valentino-vm@users.noreply.github.com>
2025-03-06 20:23:25 -0600

    Cors Configuration

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/CorsConfig.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/CorsConfig.java
@@ -1,16 +1,14 @@
 package com.springboot.MyTodoList.config;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-
-import java.util.Collections;
-import java.util.List;
 /*
     This class configures CORS, and specifies which methods are allowed
     along with which origins and headers
@@ -23,11 +21,12 @@ public class CorsConfig {
     public CorsFilter corsFilter(){
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:3000","https://objectstorage.us-phoenix-1.oraclecloud.com",
-                "https://petstore.swagger.io"));
+                "https://petstore.swagger.io", "http://160.34.211.121")); // URL permitidas
         config.setAllowedMethods(List.of("GET","POST","PUT","OPTIONS","DELETE","PATCH"));
         config.setAllowedOrigins(Collections.singletonList("*"));
         config.addAllowedHeader("*");
         config.addExposedHeader("location");
+        config.setAllowCredentials(true); // Permitir credenciales en caso de ser necesario
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         CorsFilter filter = new CorsFilter(source);

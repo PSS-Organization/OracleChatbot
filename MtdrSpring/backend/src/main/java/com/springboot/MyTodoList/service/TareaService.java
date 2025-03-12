aec5314c957d6b2733afadeb5ef749d39faf2240
valentino-vm <131170839+valentino-vm@users.noreply.github.com>
2025-03-12 01:03:47 -0600

    Tarea CRUD

---
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/service/TareaService.java
@@ -0,0 +1,83 @@
+package com.springboot.MyTodoList.service;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.springboot.MyTodoList.model.Tarea;
+import com.springboot.MyTodoList.repository.TareaRepository;
+
+@Service
+public class TareaService {
+
+    @Autowired
+    private TareaRepository tareaRepository;
+
+    public List<Tarea> getAllTareas() {
+        return tareaRepository.findAll();
+    }
+
+    public ResponseEntity<Tarea> getTareaById(Long id) {
+        Optional<Tarea> tareaData = tareaRepository.findById(id);
+        if (tareaData.isPresent()) {
+            return new ResponseEntity<>(tareaData.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    public Tarea createTarea(Tarea tarea) {
+        tarea.setFechaCreacion(OffsetDateTime.now());
+        if (tarea.getCompletado() == null) {
+            tarea.setCompletado(false);
+        }
+        if (tarea.getPrioridad() == null) {
+            tarea.setPrioridad("BAJA");
+        }
+        return tareaRepository.save(tarea);
+    }
+
+    public Tarea updateTarea(Long id, Tarea tarea) {
+        Optional<Tarea> tareaData = tareaRepository.findById(id);
+        if (tareaData.isPresent()) {
+            Tarea existingTarea = tareaData.get();
+            existingTarea.setTareaNombre(tarea.getTareaNombre());
+            existingTarea.setDescripcion(tarea.getDescripcion());
+            existingTarea.setFechaEntrega(tarea.getFechaEntrega());
+            existingTarea.setCompletado(tarea.getCompletado());
+            existingTarea.setPrioridad(tarea.getPrioridad());
+            existingTarea.setHorasEstimadas(tarea.getHorasEstimadas());
+            existingTarea.setHorasReales(tarea.getHorasReales());
+            existingTarea.setUsuarioID(tarea.getUsuarioID());
+            existingTarea.setSprintID(tarea.getSprintID());
+            existingTarea.setEstadoID(tarea.getEstadoID());
+            return tareaRepository.save(existingTarea);
+        }
+        return null;
+    }
+
+    public boolean deleteTarea(Long id) {
+        try {
+            tareaRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public List<Tarea> getTareasByUsuario(Long usuarioID) {
+        return tareaRepository.findByUsuarioID(usuarioID);
+    }
+
+    public List<Tarea> getTareasBySprint(Long sprintID) {
+        return tareaRepository.findBySprintID(sprintID);
+    }
+
+    public List<Tarea> getTareasByEstado(Long estadoID) {
+        return tareaRepository.findByEstadoID(estadoID);
+    }
+}

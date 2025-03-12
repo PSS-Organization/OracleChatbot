e362b0e5344a80fe306eea60cbdc201058be38fe
RochoMerino <a00836396@tec.mx>
2025-03-12 09:15:33 -0600

    config

--- MtdrSpring/backend/src/main/frontend/tailwind.config.js
+++ MtdrSpring/backend/src/main/frontend/tailwind.config.js
@@ -1,12 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+  content: ["./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"
+
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        brand_royal_blue: "#2A64F6", // Royal Blue
+        brand_taupe_gray: "#8E8E93", // Taupe Gray
+        brand_licorice: "#0B0014", // Licorice
+        brand_french_gray: "#AEAEB2", // French Gray
+        brand_white: "#FFFFFF", // White
+      }
+
+      ,
+    }
+
+    ,
+  }
+
+  ,
   plugins: [],
 }
 
